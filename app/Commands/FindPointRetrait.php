71c264f9360613978e6c7b9ca332c7f948537816
Cedric Puchalver <cedric@season-of-mist.com>
2025-02-28 08:15:04 +0100

    Ajout de la colonne Localité Supression du prompt pour le poids

--- app/Commands/FindPointRetrait.php
+++ app/Commands/FindPointRetrait.php
@@ -6,7 +6,6 @@ use App\Http\Integrations\Colissimo\ApiConnector;
 use App\Http\Integrations\Colissimo\Requests\FindRDVPointRetraitAcheminement;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
-use Illuminate\Support\Facades\Validator;
 use LaravelZero\Framework\Commands\Command;
 use Saloon\XmlWrangler\XmlReader;
 
@@ -35,27 +34,18 @@ class FindPointRetrait extends Command implements PromptsForMissingInput
         $config = $this->getConfig();
 
         // Ask for user input
-        $address = $this->ask('What is the address?', '12 rue Ernest Renan', ['required']);
-        $zipCode = $this->ask('What is the zip code?', '13005');
-        $city = $this->ask('What is the city?', 'Marseille');
-        $countryCode = $this->choice('What is the country code?', array_keys($config['country']), 0);
+        $address = $this->ask("Entrez l'adresse du client", '12 rue Ernest Renan', ['required']);
+        $zipCode = $this->ask('Entrez le code postal', '13005');
+        $city = $this->ask('Entrez la ville', 'Marseille');
+        $countryCode = $this->choice('Entrez le pays', array_keys($config['country']), 0);
         $optionInter = $config['country'][$countryCode]['option_inter'];
-        $filterRelay = ($countryCode === 'FR') ? 1 : 0;
-        $weight = $this->ask('What is the weight in grams?', '250');
-
-        // Validate the weight
-        $validator = Validator::make([
-            $weight,
-        ], [
-            'weight' => ['max:'.$config['country'][$countryCode]['max_weight']],
-        ]);
-        if ($validator->fails()) {
-            $validator->errors()->all();
-        }
+        $filter = $this->choice('Filtrez les résultats', array_values($config['filter']), 1);
+        $filterRelay = array_search($filter, $config['filter']);
+        // $weight = $this->ask('Entrez le poids du colis', '250');
 
         // Send the request
         $apiConnector = new ApiConnector;
-        $request = new FindRDVPointRetraitAcheminement($address, $zipCode, $city, $countryCode, $filterRelay, $optionInter, $weight);
+        $request = new FindRDVPointRetraitAcheminement($address, $zipCode, $city, $countryCode, $filterRelay, $optionInter, $weight = 1);
         $response = $apiConnector->send($request);
 
         // Handle the response
@@ -79,6 +69,7 @@ class FindPointRetrait extends Command implements PromptsForMissingInput
                             $data['adresse1'],
                             $data['adresse2'],
                             $data['adresse3'],
+                            $data['localite'],
                             $data['codePostal'],
                             $data['codePays'],
                         ];
@@ -86,7 +77,7 @@ class FindPointRetrait extends Command implements PromptsForMissingInput
 
                     // Display the table
                     $this->table(
-                        ['Identifiant', 'Nom', 'Adresse 1', 'Adresse 2', 'Adresse 3', 'Code postal', 'Code pays'],
+                        ['Identifiant', 'Nom', 'Adresse 1', 'Adresse 2', 'Adresse 3', 'Localité', 'Code postal', 'Code pays'],
                         $table,
                     );
                 }
@@ -111,85 +102,109 @@ class FindPointRetrait extends Command implements PromptsForMissingInput
         return [
             'country' => [
                 'FR' => [
+                    'name' => 'France',
                     'option_inter' => '0',
                     'max_weight' => 30000,
                 ],
                 'BE' => [
+                    'name' => 'Belgique',
                     'option_inter' => '2',
                     'max_weight' => 20000,
                 ],
                 'NL' => [
+                    'name' => 'Pays-Bas',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'DE' => [
+                    'name' => 'Allemagne',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'GB' => [
+                    'name' => 'Angleterre',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'IT' => [
+                    'name' => 'Italie',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'IE' => [
+                    'name' => 'Irlande',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'LU' => [
+                    'name' => 'Luxembourg',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'ES' => [
+                    'name' => 'Espagne',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'PT' => [
+                    'name' => 'Portugal',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'AT' => [
+                    'name' => 'Autriche',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'LT' => [
+                    'name' => 'Lituanie',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'LV' => [
+                    'name' => 'Lettonie',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'EE' => [
+                    'name' => 'Estonie',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'SE' => [
+                    'name' => 'Suede',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'PL' => [
+                    'name' => 'Pologne',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'DK' => [
+                    'name' => 'Danemark',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'HU' => [
+                    'name' => 'Hongrie',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'CZ' => [
+                    'name' => 'République tchèque',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
                 'SK' => [
+                    'name' => 'SK',
                     'option_inter' => '1',
                     'max_weight' => 20000,
                 ],
+            ],
+            'filter' => [
+                '0' => 'Bureaux de poste seulement',
+                '1' => 'Bureaux de poste et boutiques',
             ],
         ];
     }
